2558b09bab46461eabd0f4b6b1ede9c64715f47c
Janusz Skonieczny <git@wooyek.com>
2017-10-26 11:17:53 +0200

    Merge branch 'ubuntu-16.04' of github.com:wooyek/geodjango into ubuntu-16.04
    # Conflicts:
    #	README.mdMerge branch 'ubuntu-16.04' of github.com:wooyek/geodjango into ubuntu-16.04
    
    # Conflicts:
    #	README.md

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-# =========================
-# Setup Postgres data base
-# =========================
-/etc/init.d/postgresql start
-
 echo "============================================="
 echo "Docker image wooyek/geodjango version: 0.9.18"
 echo "============================================="
-echo "------> Setting up Postgres database"
+echo "------> Starting and setting up Postgres database"
+/etc/init.d/postgresql start
 # First you need to enable postgis for all new databases. This will remove superuser requirement during db initialization
 # http://stackoverflow.com/a/35209186/260480
 sudo -u postgres -E psql -d template1 -c "CREATE EXTENSION IF NOT EXISTS postgis;"
